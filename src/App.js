55950336a6329b6691ccf009c7944c270a5389d7
Thoi Dai So 2017 <thoidaiso2017@gmail.com>
2017-09-23 22:02:32 +0700

    test travis 1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Nguyen Xuan Thao V3
+          Nguyen Xuan Thao V4
         </p>
       </div>
     );
